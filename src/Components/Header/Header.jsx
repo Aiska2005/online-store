2f2441503631611c9e736fa03c70c62c18d3fc55
Aiska2005 <aiskajumantaeva5@gmail.com>
2024-11-23 13:22:02 -0600

    added data in Headers

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,32 +1,30 @@
 import React from "react";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
 
 const Header = () => {
   return (
     <header className="text-gray-600 body-font">
       <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
         <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
-          <span className="ml-3 text-xl">Tailblocks</span>
+          <span className="ml-3 text-xl">Bulut</span>
         </a>
         <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-          <a className="mr-5 hover:text-gray-900">First Link</a>
-          <a className="mr-5 hover:text-gray-900">Second Link</a>
-          <a className="mr-5 hover:text-gray-900">Third Link</a>
-          <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+          <a className="mr-5 hover:text-gray-900">On Sale</a>
+          <a className="mr-5 hover:text-gray-900">New Arrivals</a>
+          <input
+            className="bg-slate-200 p-1 rounded"
+            placeholder="type"
+            type="text"
+            size="40"
+            name="one-line"
+          />
         </nav>
-        <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Button
-          <svg
-            fill="none"
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            className="w-4 h-4 ml-1"
-            viewBox="0 0 24 24"
-          >
-            <path d="M5 12h14M12 5l7 7-7 7"></path>
-          </svg>
-        </button>
+
+        <div className="inline-flex gap-3 items-center border-0 py-1 px-3 rounded text-base mt-4 md:mt-0">
+          <AiOutlineShoppingCart />
+          <AiOutlineUser />
+        </div>
       </div>
     </header>
   );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./Components/Layout/Layout";
 import "./Styles/index.css";
+import Cards from "./Components/Cards/Cards";
 
 ReactDOM.render(
   <BrowserRouter
@@ -12,7 +13,7 @@ ReactDOM.render(
   >
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/test" element={<h4>Test</h4>} />
+        <Route path="/test" element={<Cards />} />
       </Route>
     </Routes>
   </BrowserRouter>,
