3335291d3289586aa5bb28b060c99f7ace94b1ae
Aiska2005 <aiskajumantaeva5@gmail.com>
2024-11-27 04:04:45 -0600

    created mobile in pay section

--- src/Components/Pay /Pay.jsx
+++ src/Components/Pay /Pay.jsx
@@ -3,9 +3,17 @@ import Cart from "../ui/cart/Cart";
 
 const Pay = () => {
   return (
-    <div className="container mx-auto px-4 md:p-0 flex mt-[30px]">
-      <Cart />
-      <Order />
+    <div className="container mx-auto px-4">
+      <h3 className=" mb-6 text-lg font-semibold">Your cart</h3>
+      <div className="flex flex-col md:flex-row gap-6 mt-8">
+        <div className="w-full md:w-2/3">
+          <Cart />
+        </div>
+
+        <div className="w-full md:w-1/3">
+          <Order />
+        </div>
+      </div>
     </div>
   );
 };
--- src/Components/ui/cart/Cart.jsx
+++ src/Components/ui/cart/Cart.jsx
@@ -3,20 +3,21 @@ import { AiFillDelete } from "react-icons/ai";
 
 const Cart = () => {
   return (
-    <div className="flex items-center justify-between border border-gray-200 rounded-lg p-4 shadow-sm bg-white mb-4 max-h-[150px] max-w-[667px]">
+    <div className="flex flex-col sm:flex-row items-center sm:justify-between border border-gray-200 rounded-lg p-4 shadow-sm bg-white mb-4 max-w-full sm:max-w-[667px]">
       <img
         src="https://i.pinimg.com/originals/5b/6e/ca/5b6eca63605bea0eeb48db43f77fa0ce.jpg"
-        alt=""
+        alt="Gradient Graphic T-shirt"
         className="w-20 h-20 rounded-lg object-cover"
       />
 
-      <div className="flex-1 ml-4">
+      <div className="flex-1 mt-4 sm:mt-0 sm:ml-4 text-center sm:text-left">
         <h3 className="text-lg font-semibold">Gradient Graphic T-shirt</h3>
         <p className="text-sm text-gray-600">Size: Large</p>
         <p className="text-sm text-gray-600">Color: White</p>
         <p className="text-lg font-bold mt-2 text-gray-800">$145</p>
       </div>
-      <div className="flex flex-col items-center gap-4">
+
+      <div className="flex flex-row sm:flex-col items-center gap-4 mt-4 sm:mt-0">
         <button className="text-red-500 text-xl hover:text-red-600">
           <AiFillDelete />
         </button>
