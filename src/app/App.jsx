f01481debf37e85b273d6f11aaef6f91e136e016
Akunov4594 <86850933+akunov45@users.noreply.github.com>
2024-12-19 23:46:34 +0600

    add admin panel

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,34 +1,38 @@
 import React from "react";
-import { Navigate, Route, Routes } from "react-router-dom";
+import {Navigate, Route, Routes} from "react-router-dom";
 import Layout from "../Components/Layout/Layout";
 import Cards from "../Components/Cards/Cards";
 import AddProduct from "../pages/admin/add-product/AddProduct";
 import LoginPage from "../pages/login-page/LoginPage";
-import ProductList from "../pages/product-list/ProductList";
 import ProductDetailPage from "../pages/ProductDetailPage/ProductDetailPage";
 import ProductAll from "../pages/ProductAll/ProductAll";
+import AdminPage from "../pages/admin";
+import ProductList from "../pages/admin/product-list/ProductList";
 
 const App = () => {
-  const isAuthenticated = !!localStorage.getItem(
-    "sb-jpzknqwwmjzjmomeamao-auth-toke"
-  );
-
-  return (
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Cards />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/list" element={<ProductList />} />
-        <Route
-          path="/add-product"
-          element={isAuthenticated ? <AddProduct /> : <Navigate to="/login" />}
-        />
-        <Route path="/product-detail/:id" element={<ProductDetailPage />} />
-        <Route path="/product-all" element={<ProductAll />} />
-        {/* <Route path="/admin/products" element={<ProductList />} /> */}
-      </Route>
-    </Routes>
-  );
+	const isAuthenticated = !!localStorage.getItem(
+		"sb-jpzknqwwmjzjmomeamao-auth-token"
+	);
+	
+	return (
+		<Routes>
+			<Route path="/" element={<Layout/>}>
+				<Route index element={<Cards/>}/>
+				<Route path="/login" element={<LoginPage/>}/>
+				<Route path="/product-detail/:id" element={<ProductDetailPage/>}/>
+				<Route path="/product-all" element={<ProductAll/>}/>
+			</Route>
+			<Route path="/admin/" element={<AdminPage/>}>
+				<Route path="products" element={<ProductList />} />
+				<Route
+					path="add-product"
+					element={isAuthenticated ? <AddProduct/> : <Navigate to="/login"/>}
+				/>
+				{/*<Route path="orders" element={<Orders />} />*/}
+				{/*<Route path="users" element={<Users />} />*/}
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
--- src/pages/admin/add-product/AddProduct.jsx
+++ src/pages/admin/add-product/AddProduct.jsx
@@ -1,19 +1,19 @@
-import {useState} from 'react';
+import { useState } from 'react';
 import productApi from '../../../service/product.api';
 import FileUploadApi from '../../../service/file-upload.api';
-import {useNavigate} from 'react-router-dom';
-import SelectField from "../../../Components/ui/select-field/SelectField";
-import InputField from "../../../Components/ui/input-field/InputField";
-import TextareaField from "../../../Components/ui/text-area/TextArea";
+import { useNavigate } from 'react-router-dom';
+import SelectField from '../../../Components/ui/select-field/SelectField';
+import InputField from '../../../Components/ui/input-field/InputField';
+import TextareaField from '../../../Components/ui/text-area/TextArea';
+import { Spin } from 'antd';
+import { LoadingOutlined } from '@ant-design/icons';
+import './AddProduct.css'; // Import the CSS file
 
 const AddProduct = () => {
 	const navigate = useNavigate();
 	
 	const [files, setFiles] = useState([]);
-	const [previewUrls, setPreviewUrls] = useState([]); // Для предварительного просмотра
-	const [uploadStatus, setUploadStatus] = useState('');
-	const fileUploadApi = new FileUploadApi('photos'); // Указываем bucket "photos" // Указываем bucket "photos"
-	
+	const [previewUrls, setPreviewUrls] = useState([]);
 	const [formData, setFormData] = useState({
 		name: '',
 		price: '',
@@ -23,88 +23,113 @@ const AddProduct = () => {
 		subcategory_id: '',
 		images: [],
 	});
-	
 	const [error, setError] = useState(null);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [uploadingImages, setUploadingImages] = useState([]); // Стейт для отслеживания загрузки изображений
 	
-	const {name, price, description, stock, category_id, subcategory_id} = formData;
+	const fileUploadApi = new FileUploadApi('photos');
 	
 	const handleInputChange = (e) => {
-		const {name, value} = e.target;
-		setFormData((prevState) => ({
-			...prevState,
-			[name]: value,
-		}));
+		const { name, value } = e.target;
+		setFormData((prevState) => ({ ...prevState, [name]: value }));
 	};
 	
 	const handleImageChange = (e) => {
 		const selectedFiles = Array.from(e.target.files);
 		
-		// Проверка, чтобы общее количество изображений не превышало 4
+		// Проверка на количество файлов
 		if (files.length + selectedFiles.length > 4) {
 			setError('Можно загрузить не более 4 изображений.');
 			setTimeout(() => setError(null), 3000);
 			return;
 		}
 		
-		// Создаем URL для предварительного просмотра новых файлов
+		// Проверка формата файлов (например, только изображения)
+		const validFileTypes = ['image/jpeg','image/jpg', 'image/png','image/webp',];
+		const invalidFiles = selectedFiles.filter(file => !validFileTypes.includes(file.type));
+		
+		if (invalidFiles.length > 0) {
+			setError('Можно загружать только изображения формата JPG, PNG или Webp.');
+			setTimeout(() => setError(null), 3000);
+			return;
+		}
+		
+		// Добавляем превью
 		const newPreviewUrls = selectedFiles.map((file) => URL.createObjectURL(file));
 		setPreviewUrls((prevPreviewUrls) => [...prevPreviewUrls, ...newPreviewUrls]);
-		
-		// Добавляем новые файлы к уже существующим
 		setFiles((prevFiles) => [...prevFiles, ...selectedFiles]);
+		
+		// Запускаем загрузку изображений
+		selectedFiles.forEach((file) => {
+			uploadImage(file);
+		});
 	};
-
+	
+	const uploadImage = async (file) => {
+		try {
+			setUploadingImages((prevUploading) => [...prevUploading, file.name]); // Добавляем файл в список загружаемых изображений
+			const path = `uploads/${file.name}`;
+			const imageUrl = await fileUploadApi.uploadFile(file, path);
+			
+			setFormData((prevData) => ({
+				...prevData,
+				images: [...prevData.images, imageUrl], // Добавляем загруженную картинку в массив изображений
+			}));
+		} catch (error) {
+			console.error('Ошибка при загрузке изображения:', error);
+			setError('Не удалось загрузить одно или несколько изображений. Попробуйте снова.');
+			setTimeout(() => setError(null), 6000);
+		} finally {
+			setUploadingImages((prevUploading) => prevUploading.filter((name) => name !== file.name)); // Убираем файл из списка загружаемых
+		}
+	};
+	
+	const handleDeleteImage = (index) => {
+		const updatedFiles = files.filter((_, idx) => idx !== index);
+		const updatedPreviewUrls = previewUrls.filter((_, idx) => idx !== index);
+		setFiles(updatedFiles);
+		setPreviewUrls(updatedPreviewUrls);
+	};
 	
 	const handleAddProduct = async () => {
-		if (!name.trim() || !price || !description.trim() || !category_id || !subcategory_id) {
+		// Проверка на заполнение всех полей
+		if (!formData.name.trim() || !formData.price || !formData.description.trim() || !formData.category_id || !formData.subcategory_id) {
 			setError('Пожалуйста, заполните все поля.');
 			setTimeout(() => setError(null), 3000);
 			return;
 		}
 		
-		const uploadedImageUrls = [];
-		
-		try {
-			for (const file of files) {
-				const path = `uploads/${file.name}`;
-				const imageUrl = await fileUploadApi.uploadFile(file, path);
-				uploadedImageUrls.push(imageUrl);
-			}
-			
-			setUploadStatus('Изображения успешно загружены.');
-		} catch (uploadError) {
-			console.error('Ошибка загрузки изображений:', uploadError);
-			setError('Ошибка загрузки изображений. Попробуйте снова.');
+		// Проверка на загрузку изображений
+		if (uploadingImages.length > 0) {
+			setError('Подождите, пока изображения загрузятся.');
 			return;
 		}
 		
-		const newProduct = {
-			name,
-			price: parseFloat(price),
-			description,
-			stock: parseInt(stock, 10),
-			category_id: parseInt(category_id, 10),
-			subcategory_id: parseInt(subcategory_id, 10),
-			images: uploadedImageUrls,
-		};
+		setIsSubmitting(true);
 		
 		try {
-			const addedProduct = await productApi.addProduct(newProduct);
-			if (addedProduct) {
-				alert('Продукт успешно добавлен');
-				resetForm();
-				navigate('/admin/products/'); // Перенаправляем пользователя
-			}
+			const newProduct = {
+				...formData,
+				price: parseFloat(formData.price),
+				stock: parseInt(formData.stock, 10),
+				category_id: parseInt(formData.category_id, 10),
+				subcategory_id: parseInt(formData.subcategory_id, 10),
+			};
+			
+			await productApi.addProduct(newProduct);
+			alert('Продукт успешно добавлен');
+			resetForm();
+			navigate('/admin/products/');
 		} catch (error) {
 			console.error('Ошибка при добавлении продукта:', error);
 			setError('Не удалось добавить продукт. Попробуйте снова.');
+		} finally {
+			setIsSubmitting(false);
 		}
 	};
 	
 	const resetForm = () => {
-		// Освобождаем созданные URL
 		previewUrls.forEach((url) => URL.revokeObjectURL(url));
-		
 		setFormData({
 			name: '',
 			price: '',
@@ -117,98 +142,111 @@ const AddProduct = () => {
 		setFiles([]);
 		setPreviewUrls([]);
 		setError(null);
-		setUploadStatus('');
+		setUploadingImages([]); // Сбросим статус загрузки изображений
 	};
-
-	
 	
 	return (
-		<div className="max-w-lg mx-auto p-6 bg-white shadow-md rounded-md">
-			<h2 className="text-2xl font-bold text-center mb-4">Добавить новый товар</h2>
+		<div className="add-product-container">
+			<h2 className="add-product-title">Добавить новый товар</h2>
 			{error && (
-				<div className="mb-4 p-3 text-red-700 bg-red-100 border border-red-300 rounded">
+				<div className="error-message">
 					{error}
 				</div>
 			)}
-			<form className="space-y-4" onSubmit={(e) => e.preventDefault()}>
-				<InputField
-					label="Название"
-					name="name"
-					value={name}
-					onChange={handleInputChange}
-				/>
-				<InputField
-					label="Цена"
-					name="price"
-					type="number"
-					value={price}
-					onChange={handleInputChange}
-				/>
-				<TextareaField
-					label="Описание"
-					name="description"
-					value={description}
-					onChange={handleInputChange}
-				/>
-				<InputField
-					label="Количество на складе"
-					name="stock"
-					type="number"
-					value={stock}
-					onChange={handleInputChange}
-				/>
-				<SelectField
-					label="Категория"
-					name="category_id"
-					value={category_id}
-					options={[
-						{value: '', label: 'Выберите категорию'},
-						{value: '1', label: 'Мужчины'},
-						{value: '2', label: 'Женщины'},
-					]}
-					onChange={handleInputChange}
-				/>
-				<SelectField
-					label="Подкатегория"
-					name="subcategory_id"
-					value={subcategory_id}
-					options={[
-						{value: '', label: 'Выберите подкатегорию'},
-						{value: '1', label: 'Футболки'},
-						{value: '2', label: 'Брюки'},
-						{value: '3', label: 'Двойка для Девушек'},
-					]}
-					onChange={handleInputChange}
-				/>
-				<InputField
-					label="Изображения (до 4 штук)"
-					name="images"
-					type="file"
-					multiple
-					onChange={handleImageChange}
-				/>
+			
+			<div className="form-container">
+				{/* Левая часть - текстовые поля */}
+				<div className="left-form-section">
+					<form onSubmit={(e) => e.preventDefault()}>
+						<div className="input-fields-container">
+							<InputField label="Название" name="name" value={formData.name} onChange={handleInputChange} />
+							<InputField label="Цена" name="price" type="number" value={formData.price} onChange={handleInputChange} />
+							<TextareaField label="Описание" name="description" value={formData.description} onChange={handleInputChange} />
+							<InputField label="Количество на складе" name="stock" type="number" value={formData.stock} onChange={handleInputChange} />
+						</div>
+						
+						<div className="input-fields-container">
+							<SelectField
+								label="Категория"
+								name="category_id"
+								value={formData.category_id}
+								options={[
+									{ value: '', label: 'Выберите категорию' },
+									{ value: '1', label: 'Мужчины' },
+									{ value: '2', label: 'Женщины' },
+								]}
+								onChange={handleInputChange}
+							/>
+							<SelectField
+								label="Подкатегория"
+								name="subcategory_id"
+								value={formData.subcategory_id}
+								options={[
+									{ value: '', label: 'Выберите подкатегорию' },
+									{ value: '1', label: 'Футболки' },
+									{ value: '2', label: 'Брюки' },
+									{ value: '3', label: 'Двойка для Девушек' },
+									{ value: '5', label: 'Платье' },
+								]}
+								onChange={handleInputChange}
+							/>
+						</div>
+					</form>
+				</div>
 				
-				{/* Отображение предварительного просмотра изображений */}
-				{previewUrls.length > 0 && (
-					<div className="mt-4 grid grid-cols-2 gap-4">
-						{previewUrls.map((url, index) => (
-							<div key={index} className="border p-2 rounded">
-								<img src={url} alt={`preview-${index}`} className="w-full h-auto"/>
-							</div>
-						))}
+				{/* Правая часть - загрузка изображений */}
+				<div className="right-form-section">
+					<div className="image-upload-section">
+						<label className="select-label">Изображения (до 3 штук)</label>
+						<input
+							name="images"
+							type="file"
+							multiple
+							onChange={handleImageChange}
+							className="input-field"
+						/>
 					</div>
-				)}
-				
-				<button
-					onClick={handleAddProduct}
-					className="w-full py-2 bg-blue-600 text-white font-bold rounded hover:bg-blue-700 transition duration-200"
-				>
-					Добавить товар
-				</button>
-				{uploadStatus && (
-					<div className="mt-2 text-green-600">{uploadStatus}</div>
-				)}
-			</form>
+					
+					{previewUrls.length > 0 && (
+						<div className="image-preview-grid">
+							{previewUrls.map((url, index) => (
+								<div key={index} className="image-preview">
+									<img src={url} alt={`preview-${index}`} />
+									<button
+										type="button"
+										onClick={() => handleDeleteImage(index)}
+										className="delete-image-button"
+									>
+										X
+									</button>
+									{uploadingImages.includes(files[index].name) && (
+										<div className="upload-spinner-overlay">
+											<Spin
+												indicator={
+													<LoadingOutlined
+														style={{
+															fontSize: 48,
+														}}
+														spin
+													/>
+												}
+											/>
+										</div>
+									)}
+								</div>
+							))}
+						</div>
+					)}
+				</div>
+			</div>
+			
+			<button
+				onClick={handleAddProduct}
+				disabled={isSubmitting}
+				className="submit-button"
+			>
+				{isSubmitting ? 'Сохранение...' : 'Добавить товар'}
+			</button>
 		</div>
 	);
 };
