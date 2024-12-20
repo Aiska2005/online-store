377b29a58a0d61b4851683330f05924597350c40
Aiska2005 <aiskajumantaeva5@gmail.com>
2024-12-20 17:18:37 -0600

    changed in cart design

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,40 +1,38 @@
 import React from "react";
-import {Navigate, Route, Routes} from "react-router-dom";
+import { Navigate, Route, Routes } from "react-router-dom";
 import Layout from "../Components/Layout/Layout";
-import AddProduct from "../pages/admin/add-product/AddProduct";
-import LoginPage from "../pages/login-page/LoginPage";
-import ProductDetailPage from "../pages/ProductDetailPage/ProductDetailPage";
-import ProductAll from "../pages/ProductAll/ProductAll";
-import AdminPage from "../pages/admin";
-import ProductList from "../pages/admin/product-list/ProductList";
-import CartPage from "../pages/cart-page/CartPage";
-import HomePage from "../pages/home-page/HomePage";
+import HomePag from "../Pages/home-page/HomePage";
+import LoginPage from "../Pages/login-page/LoginPage";
+import ProductDetailPage from "../Pages/ProductDetailPage/ProductDetailPage";
+import ProductAll from "../Pages/ProductAll/ProductAll";
+import CartPage from "../Pages/cart-page/CartPage";
+// import AdminPage from "../Pages/admin/add-product";
 
 const App = () => {
-	const isAuthenticated = !!localStorage.getItem(
-		"sb-jpzknqwwmjzjmomeamao-auth-token"
-	);
-	
-	return (
-		<Routes>
-			<Route path="/" element={<Layout/>}>
-				<Route index element={<HomePage/>}/>
-				<Route path="/login" element={<LoginPage/>}/>
-				<Route path="/product-detail/:id" element={<ProductDetailPage/>}/>
-				<Route path="/product-all" element={<ProductAll/>}/>
-				<Route path="/cart" element={<CartPage/>}/>
-			</Route>
-			<Route path="/admin/" element={<AdminPage/>}>
-				<Route path="products" element={<ProductList />} />
-				<Route
-					path="add-product"
-					element={isAuthenticated ? <AddProduct/> : <Navigate to="/login"/>}
-				/>
-				{/*<Route path="orders" element={<Orders />} />*/}
-				{/*<Route path="users" element={<Users />} />*/}
-			</Route>
-		</Routes>
-	);
+  const isAuthenticated = !!localStorage.getItem(
+    "sb-jpzknqwwmjzjmomeamao-auth-token"
+  );
+
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePag />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/product-detail/:id" element={<ProductDetailPage />} />
+        <Route path="/product-all" element={<ProductAll />} />
+        <Route path="/cart" element={<CartPage />} />
+      </Route>
+      {/* <Route path="/admin/" element={<AdminPage />}> */}
+      {/* <Route path="products" element={<ProductList />} /> */}
+      {/* <Route
+          path="add-product"
+          element={isAuthenticated ? <AddProduct /> : <Navigate to="/login" />}
+        /> */}
+      {/*<Route path="orders" element={<Orders />} />*/}
+      {/*<Route path="users" element={<Users />} />*/}
+      {/* </Route> */}
+    </Routes>
+  );
 };
 
 export default App;
--- src/pages/ProductAll/ProductAll.jsx
+++ src/pages/ProductAll/ProductAll.jsx
@@ -29,32 +29,169 @@ const ProductAll = () => {
       {/* Product List */}
       <section className="text-gray-600 body-font w-full">
         <div className="container px-2 sm:px-5 mx-auto">
-          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
             {goods.length > 0 ? (
               goods.map((item) => (
-                <div key={item.id} className="p-2">
+                <div
+                  key={item.id}
+                  className="rounded-lg border border-gray-200 p-2 shadow-sm"
+                >
                   <a
                     href={`/product-detail/${item.id}`}
-                    className="block relative h-64 rounded overflow-hidden"
+                    className="block relative h-96 w-full flex items-center justify-center overflow-hidden rounded-lg bg-gray-100"
                   >
                     <img
-                      alt="ecommerce"
-                      className="object-contain object-center w-full h-full block"
+                      className="object-cover h-full w-full"
                       src={item.images?.[0] || "https://dummyimage.com/420x260"}
+                      alt={item.name}
                     />
                   </a>
-                  <div className="mt-2 text-center">
-                    <h2 className="text-gray-900 title-font text-sm sm:text-lg font-medium">
-                      {item.name}
-                    </h2>
-                    <p className="mt-1 text-gray-700">${item.price || "N/A"}</p>
+                  <div className="pt-2">
+                    <div className="mb-4 flex items-center justify-between gap-4">
+                      <div className="mt-2 flex items-center gap-2">
+                        <div className="flex items-center">
+                          {[...Array(5)].map((_, index) => (
+                            <svg
+                              key={index}
+                              className="h-4 w-4 text-yellow-400"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="currentColor"
+                              viewBox="0 0 24 24"
+                            >
+                              <path d="M13.8 4.2a2 2 0 0 0-3.6 0L8.4 8.4l-4.6.3a2 2 0 0 0-1.1 3.5l3.5 3-1 4.4c-.5 1.7 1.4 3 2.9 2.1l3.9-2.3 3.9 2.3c1.5 1 3.4-.4 3-2.1l-1-4.4 3.4-3a2 2 0 0 0-1.1-3.5l-4.6-.3-1.8-4.2Z" />
+                            </svg>
+                          ))}
+                        </div>
+                        <p className="text-sm font-medium">5.0</p>
+                      </div>
+                      <div className="flex items-center justify-end gap-1">
+                        <button
+                          type="button"
+                          title="Quick look"
+                          className="rounded-lg p-2"
+                        >
+                          <a href={`/product-detail/${item.id}`}>
+                            <span className="sr-only">Quick look</span>
+                            <svg
+                              className="h-5 w-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z"
+                              />
+                              <path
+                                stroke="currentColor"
+                                strokeWidth="2"
+                                d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                              />
+                            </svg>
+                          </a>
+                        </button>
+
+                        <button
+                          type="button"
+                          title="Add to favorites"
+                          className="rounded-lg p-2"
+                        >
+                          <span className="sr-only">Add to favorites</span>
+                          <svg
+                            className="h-5 w-5"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                          >
+                            <path
+                              stroke="currentColor"
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              strokeWidth="2"
+                              d="M12 6C6.5 1 1 8 5.8 13l6.2 7 6.2-7C23 8 17.5 1 12 6Z"
+                            />
+                          </svg>
+                        </button>
+                      </div>
+                    </div>
+                    <div className="flex flex-col justify-between">
+                      <div>
+                        <p className="text-lg font-semibold leading-tight mb-2 line-clamp-2">
+                          {item.name}
+                        </p>
+                      </div>
+                      <div>
+                        <ul className="mt-2 flex items-center gap-4">
+                          <li className="flex items-center gap-2">
+                            <svg
+                              className="h-4 w-4"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M13 7h6l2 4m-8-4v8m0-8V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v9h2m8 0H9m4 0h2m4 0h2v-4m0 0h-5m3.5 5.5a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Zm-10 0a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0Z"
+                              />
+                            </svg>
+                            <p className="text-sm font-medium">
+                              Быстрая доставка
+                            </p>
+                          </li>
+                          <li className="flex items-center gap-2">
+                            <svg
+                              className="h-4 w-4"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeWidth="2"
+                                d="M8 7V6c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1h-1M3 18v-7c0-.6.4-1 1-1h11c.6 0 1 .4 1 1v7c0 .6-.4 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z"
+                              />
+                            </svg>
+                            <p className="text-sm font-medium">Лучшая цена</p>
+                          </li>
+                        </ul>
+                        <div className="mt-4 flex items-center justify-between gap-4">
+                          <p className="text-2xl font-extrabold leading-tight">
+                            ${item.price || "N/A"}
+                          </p>
+
+                          <button
+                            type="button"
+                            className="inline-flex items-center rounded-lg border bg-primary-700 px-5 py-2.5 text-sm font-medium text-black hover:bg-primary-800 focus:outline-none"
+                          >
+                            <svg
+                              className="-ms-2 me-2 h-5 w-5"
+                              xmlns="http://www.w3.org/2000/svg"
+                              fill="none"
+                              viewBox="0 0 24 24"
+                            >
+                              <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M4 4h1.5L8 16m0 0h8m-8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm8 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4Zm.75-3H7.5M11 7H6.312M17 4v6m-2-2h4"
+                              />
+                            </svg>
+                            <span className="ml-1">Купить</span>
+                          </button>
+                        </div>
+                      </div>
+                    </div>
                   </div>
                 </div>
               ))
             ) : (
-              <p className="text-center w-full text-gray-500">
-                Нет доступных товаров
-              </p>
+              <p className="text-center text-gray-500">No products found.</p>
             )}
           </div>
         </div>
